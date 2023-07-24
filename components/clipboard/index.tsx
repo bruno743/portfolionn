6e7b9b1409b8b4fc904107af5283aba035600873
bruno743 <57687873+bruno743@users.noreply.github.com>
2023-07-24 20:05:17 -0300

    Update index.tsx clipboard

--- components/clipboard/index.tsx
+++ components/clipboard/index.tsx
@@ -12,11 +12,11 @@ const CopyBoard = () => {
     <div className="pt-24 justify-center grid grid-rows-2 font-semibold text-lg text-end bg-white bg-clip-text text-transparent">
       <div className="w-[325px] flex flex-col space-y-6">
         <div className="flex flex-row justify-between">
-          <Image className="" src="/images/telegram.svg" width={32} height={32} alt="telegram" />
+          <Image className="" src="./images/telegram.svg" width={32} height={32} alt="telegram" />
           <span className="text-end" >{telefone}</span>
         </div>
         <div className="flex flex-row  justify-between">
-          <Image className="" src="/images/mail.svg" width={32} height={32} alt="mail" />
+          <Image className="" src="./images/mail.svg" width={32} height={32} alt="mail" />
           <span className="text-end">{email}</span>
         </div>
       </div>
