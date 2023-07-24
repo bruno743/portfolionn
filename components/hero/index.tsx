3675f2c857f94e28881c9b980dfc157d269c5324
bruno743 <57687873+bruno743@users.noreply.github.com>
2023-07-24 19:51:54 -0300

    Update index.tsx hero

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -12,6 +12,7 @@ const Hero = () => {
           <span className="ml-3 whitespace-nowrap bg-sky-600 bg-clip-text text-transparent">
             {hero.name}
           </span>
+          .
         </h1>
         <h1 className="text-6xl font-bold tracking-wide mt-4">{hero.subtitle}</h1>
         <a href="#About" className="text-2xl font-bold p-0.5 mt-6 w-44 bg-sky-600">
